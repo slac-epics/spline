4b4a360b5719a2e7587da91a2f2dae526a086ac2
Alex Montironi <alexmon@slac.stanford.edu>
2020-04-09 14:36:47 -0700

    Modified st.cmd to demonstrate spline module usage

--- iocBoot/iocspline/st.cmd
+++ iocBoot/iocspline/st.cmd
@@ -1,28 +1,46 @@
-#!../../bin/linux-x86/spline
-
-## You may have to change spline to something else
-## everywhere it appears in this file
+#!../../bin/rhel6-x86_64/umc
+## --------------------------------------------------------------------------------
 
 < envPaths
-
-
 cd ${TOP}
 
+## --------------------------------------------------------------------------------
+
 ## Register all support components
-dbLoadDatabase("dbd/spline.dbd")
-spline_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase "dbd/umc.dbd"
+umc_registerRecordDeviceDriver pdbbase
 
+## --------------------------------------------------------------------------------
+# Set environment variables
+## -------------------------------------------------------------------------------
 
-# ====================================================================
-# Setup some additional environment variables
-# ====================================================================
-# Setup environment variables
-epicsEnvSet("ENGINEER","Jacob DeFilippis")
-epicsEnvSet("LOCATION","??")
-epicsEnvSet("IOC_DATA","/nfs/slac/g/lcls/epics/ioc/data")
-# =====================================================================
+# Stream device protocol file path
+epicsEnvSet( "STREAM_PROTOCOL_PATH", "$(TOP)/umcApp/src" )
+
+# IOC and PV prefix 
+epicsEnvSet("IOC_NAME", "SIOC:TEST:SPL")
+epicsEnvSet("LOCATION", "B34")
+epicsEnvSet("SEGMENT" , "TST1")
+epicsEnvSet("PREFIX"  , "USEG:${LOCATION}:${SEGMENT}")
+
+epicsEnvSet("ENGINEER" , "Alex Montironi")
+
+## --------------------------------------------------------------------------------
+
+## SPLINE INIT
+#   (1) name of the spline transformation [use this in template macro TNAME]
+#   (2) path to file with spline input data [absolute path]
+splineInit("k2gap","iocBoot/datafiles/sxuXXX_k2gap.dat")
+
+## --------------------------------------------------------------------------------
+
 ## Load record instances
-dbLoadRecords("db/k_to_gap.db","user=jpdefHost")
 
-iocInit()
+dbLoadRecords(db/splineTesting.db, "U=${PREFIX}")
+
+## --------------------------------------------------------------------------------
+
+cd ${TOP}/iocBoot/${IOC}
+iocInit
+
 
