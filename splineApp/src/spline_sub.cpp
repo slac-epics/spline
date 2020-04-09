544f740c2fc33d2c73565b4b902df81c02101bf0
Alex Montironi <alexmon@slac.stanford.edu>
2020-04-09 14:20:35 -0700

    Renamed functions, improved data types consistency, added splineGetInpPrms

--- splineApp/src/spline_sub.cpp
+++ splineApp/src/spline_sub.cpp
@@ -1,42 +1,10 @@
-/*Standard libs*/
-#include <stdio.h>
-#include <stdarg.h>
-#include <string>
-
-/*EPICS libs*/
-#include <iocsh.h>
-#include <epicsExport.h>
-#include <registryFunction.h>
-#include <dbDefs.h>
-#include <aSubRecord.h>
-#include <dbCommon.h>
-#include <recSup.h>
-#include <dbAccess.h>
-#include <ap.h>
-#include "iocsh.h"
-#include <epicsExport.h>
-#include <epicsMutex.h>
-#include <cantProceed.h>        /* for callocMustSucceed()             */
-#include <dbFldTypes.h>         /* for DBF_DOUBLE                      */
-
 /*Local libs*/
-#include "spline_interp.h"
+#include "spline_sub.h"
 
 /* Define SUCCESS macro*/
 #ifndef SUCCESS
 #define SUCCESS 1
 #endif
-
-typedef std::vector< std::pair<std::string, spline> > SplineContainer;
-
-typedef struct devicePvt_s
-{
-   epicsUInt32   npts;
-   epicsMutexId  mlock;
-}devicePvt_ts;
-
-/*Main data structure*/
-SplineContainer scon;
 
 /*
 * spline getSplineFromContainer(aSubRecord* psub){
@@ -58,7 +26,7 @@ static spline getSplineFromContainer(std::string psub){
  * Generic spline initialization function 
  * To be used in the INAM field
 */
-static long splInit(aSubRecord *psub) {
+static long splineInit(aSubRecord *psub) {
   long status = SUCCESS;
   devicePvt_ts *dpvt_ps = NULL;
   /* Check that this pv has not already been initialized */
@@ -73,13 +41,13 @@ static long splInit(aSubRecord *psub) {
 
 /*
 *
-* splineIt(struct subRecord *psub)
+* splineCalcOutput(struct subRecord *psub)
 * -calls spline function and sets record value
 * to interpolation at point A
 * - VAL = f(A)
 *
 */
-static long splineIt(aSubRecord *psub){
+static long splineCalcOutput(aSubRecord *psub){
   spline s;
 
   
@@ -129,11 +97,11 @@ static long splineIt(aSubRecord *psub){
 
 /*
 *
-* getLimits(struct subRecord *psub)
-* - get limits on input data
+* splineGetLimits(struct subRecord *psub)
+* - retrieve max and min values of data points used to build spline 
 *
 */
-static long getLimits(aSubRecord *psub){
+static long splineGetLimits(aSubRecord *psub){
   spline s;
 
   
@@ -183,16 +151,17 @@ static long getLimits(aSubRecord *psub){
 
 /*
 *
-* getNumPoints(struct subRecord *psub)
-* - get number of data points in input data
+* splineGetNumPoints(struct subRecord *psub)
+* - get number of data points used to buld spline 
 *
 */
-static long getNumPoints(aSubRecord *psub){
+static long splineGetNumPoints(aSubRecord *psub){
   spline s;
 
   
   //Cast EPICS fields to correct types
-  double outa[1]; 
+  //epicsInt32 outa[1]; 
+  epicsInt32 numDataPoints; 
   char* inpa; 
   inpa = (char*) psub->a;
    
@@ -215,9 +184,11 @@ static long getNumPoints(aSubRecord *psub){
 
   } else {
     /*Fetch number of data points*/
-    outa[0] = s.get_num_points(); 
-    *(double *)(psub->vala) = outa[0];
-    printf("Num points: %.0f\n", outa[0]); 
+    //outa[0] = s.get_num_points(); 
+    //*(epicsInt32 *)(psub->vala) = outa[0];
+    numDataPoints = s.get_num_points(); 
+    *(epicsInt32 *)(psub->vala) = numDataPoints;
+    printf("Num points: %d\n", numDataPoints); 
   }
   
   return 0;    
@@ -225,11 +196,11 @@ static long getNumPoints(aSubRecord *psub){
 
 /*
 *
-* getPoints(struct subRecord *psub)
+* splineGetPoints(struct subRecord *psub)
 * - get data points from input file (points stored in memory used for interpolation)
 *
 */
-static long getPoints(aSubRecord *psub){
+static long splineGetPoints(aSubRecord *psub){
   spline s;
   // Pointers to hold the returned values
   double* xpts = NULL; 
@@ -307,11 +278,11 @@ static long getPoints(aSubRecord *psub){
 
 /*
 *
-* getDate(struct subRecord *psub)
-* - get date from file 
+*splineGetDate(struct subRecord *psub)
+* - get date from calibration file 
 *
 */
-static long getDate(aSubRecord *psub){
+static long splineGetDate(aSubRecord *psub){
   spline s;
   // Pointers to hold the returned values
   char* date = NULL; 
@@ -339,9 +310,9 @@ static long getDate(aSubRecord *psub){
 
   } else {
      
-     /* Fetch the data points */
+     /* Fetch the date */
      s.get_date(date);
-     /* Assign data points to the output*/
+     /* Assign date to the output*/
     for(int i = 0; i < DATEFILE_CHARS; i++) {
       if(date[i]  == '\0' || date[i] == '\r') {
          break;
@@ -354,6 +325,112 @@ static long getDate(aSubRecord *psub){
   
    return 0;    
 }
+
+/* 
+ * splineGetInpPrms(struct subRecord *psub)
+ * - Get all input parameters that make up the spline used for interpolation
+ * - OUTA: Number of data points (LONG)
+ * - OUTB: Data points in X array (DOUBLE - waveform)
+ * - OUTC: Data points in Y array (DOUBLE - waveform)
+ * - OUTD: Date when the calibration file was saved (STRING)
+*/ 
+static long splineGetInpPrms(aSubRecord *psub){
+  spline s;
+  // Pointers to hold the returned values
+  char* date = NULL; 
+  epicsInt32 numDataPoints; 
+  double* xpts = NULL; 
+  double* ypts = NULL; 
+  int npts = 0;
+  int xnpts = 0;
+  int ynpts = 0;
+  devicePvt_ts  *dpvt_ps = NULL;
+  size_t nbytes = sizeof(double) * psub->nova;
+
+  //Cast EPICS fields to correct types
+  char* inpa; 
+  inpa = (char*) psub->a;
+  
+  dpvt_ps = (devicePvt_ts *)psub->dpvt;
+  s = getSplineFromContainer(std::string(inpa));
+  /*If this is first call then initialize
+  the spline*/
+  if ( ! s.is_initialized() ) {
+    try{
+          printf("No such transformation %s\n",inpa);
+          return -1;
+    }catch (int e) {
+      if( e < 0 ) {
+          printf("Encoutered error please check data for syntax errors, and discontinuities\n");
+        return e;
+      }
+    } catch (alglib::ap_error a) {
+          printf("Encoutered error please check data for syntax errors, and discontinuities\n");
+      return -1;
+    }
+
+  } else {
+  
+  /* Fetch the number of data points */   
+  numDataPoints = s.get_num_points(); 
+  *(epicsInt32 *)(psub->vala) = numDataPoints;
+  printf("Num points splineGetInpParams: %d\n", numDataPoints); 
+     
+  /* Fetch the data points */
+  /* clear waveform */
+  epicsMutexMustLock(dpvt_ps->mlock); 
+  memset(psub->valb,0,nbytes);
+  epicsMutexUnlock(dpvt_ps->mlock);
+  psub->nevb=0;
+     
+  epicsMutexMustLock(dpvt_ps->mlock); 
+  memset(psub->valc,0,nbytes);
+  epicsMutexUnlock(dpvt_ps->mlock);
+  psub->nevc=0;
+     
+  /* Fetch the data points */
+  s.get_X_array(xpts, &xnpts);
+  s.get_Y_array(ypts, &ynpts);
+  /* Make sure we have retrieved the same number of points from the two arrays*/
+  if(xnpts == ynpts) {
+      npts = xnpts;
+  }
+  else {
+     printf("X and Y number of data points differ. Exiting. xnpts: %d, ynpts: %d\n", xnpts, ynpts);
+     return 1;
+  }
+  /* Assign data points to the output*/
+  for (int i = 0; i < npts; i++) {
+      epicsMutexMustLock(dpvt_ps->mlock)
+      ((double*)psub->valb)[i] = xpts[i];
+      epicsMutexUnlock(dpvt_ps->mlock);
+  }
+  psub->nevb = npts;
+     
+ /* Assign data points to the output*/
+ for (int i = 0; i < npts; i++) {
+     epicsMutexMustLock(dpvt_ps->mlock)
+     ((double*)psub->valc)[i] = ypts[i];
+     epicsMutexUnlock(dpvt_ps->mlock);
+ }
+ psub->nevc = npts;
+     
+ /* Fetch the date when the file was saved */
+ s.get_date(date);
+ /* Assign all the character to the output*/
+ for(int i = 0; i < DATEFILE_CHARS; i++) {
+     if(date[i]  == '\0' || date[i] == '\r') {
+         break;
+      }
+      else {
+        ((char*)psub->vald)[i]  = date[i];
+      }
+    }
+   }
+  
+   return 0;    
+}
+
 
 /*
 This block sets up a interface for the ioc shell. It allows the user to intializes
@@ -379,10 +456,11 @@ static void drvSplineRegistrar(){
 }
 
 
-epicsRegisterFunction(splineIt);
-epicsRegisterFunction(splInit);
-epicsRegisterFunction(getLimits);
-epicsRegisterFunction(getNumPoints);
-epicsRegisterFunction(getPoints);
-epicsRegisterFunction(getDate);
+epicsRegisterFunction(splineCalcOutput);
+epicsRegisterFunction(splineInit);
+epicsRegisterFunction(splineGetLimits);
+epicsRegisterFunction(splineGetNumPoints);
+epicsRegisterFunction(splineGetPoints);
+epicsRegisterFunction(splineGetDate);
+epicsRegisterFunction(splineGetInpPrms);
 epicsExportRegistrar(drvSplineRegistrar);
--- splineApp/src/spline_sub.h
+++ splineApp/src/spline_sub.h
@@ -16,6 +16,11 @@
 #include <recSup.h>
 #include <dbAccess.h>
 #include <ap.h>
+#include "iocsh.h"
+#include <epicsExport.h>
+#include <epicsMutex.h>
+#include <cantProceed.h>        /* for callocMustSucceed()             */
+#include <dbFldTypes.h>         /* for DBF_DOUBLE                      */
 
 /*Local libs*/
 #include "spline_interp.h"
@@ -24,16 +29,23 @@
 typedef std::vector< std::pair<std::string, spline> > SplineContainer;
 
 /*Main data structure*/
-  SplineContainer scon;
+SplineContainer scon;
 
-static std::string  makePath(const char* filename_c_str);
-static void initSpline(std::string trans_name, std::string filepath);
+typedef struct devicePvt_s
+{
+   epicsUInt32   npts;
+   epicsMutexId  mlock;
+}devicePvt_ts;
+
+//static std::string  makePath(const char* filename_c_str);
+//static void initSpline(std::string trans_name, std::string filepath);
 static spline getSplineFromContainer(std::string psub);
-static long splineIt(aSubRecord *psub);
-static long splInit(aSubRecord *psub);
-static long getLimits(aSubRecord *psub);
-static long getNumPoints(aSubRecord *psub);
-static long getPoints(aSubRecord *psub);
-static long getDate(aSubRecord *psub);
+static long splineCalcOutput(aSubRecord *psub);
+static long splineInit(aSubRecord *psub);
+static long splineGetLimits(aSubRecord *psub);
+static long splineGetNumPoints(aSubRecord *psub);
+static long splineGetPoints(aSubRecord *psub);
+static long splineGetDate(aSubRecord *psub);
+static long splineGetInpPrms(aSubRecord *psub);
 
 #endif
